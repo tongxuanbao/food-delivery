6452dc9dd865fc25fc64625b3c283fc4b162d4ab
Ryan Tong <baotongxuan@gmail.com>
2024-01-06 13:21:25 +1100

    demo: change user to olivia

--- user/main.go
+++ user/main.go
@@ -36,7 +36,7 @@ func main() {
 	}
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong")
+		fmt.Fprintf(w, "Olivia\n")
 	})
 
 	http.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
