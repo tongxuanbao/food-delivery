005227e25f2e502816732a92882689a0fc3d030c
Ryan Tong <baotongxuan@gmail.com>
2024-01-27 15:16:32 +1100

    Demo: test

--- TODO.md
+++ TODO.md
@@ -1,1 +1,6 @@
-- Create front end -> where is the front end
+## Todo
+- [ ] draw a 4x4 square, and a dot move to each square every 3 seconds
+
+
+## Done
+- [x] Create front end -> where is the front end
--- simulator/main.go
+++ simulator/main.go
@@ -19,6 +19,38 @@ func main() {
 		fmt.Fprintf(w, "<button>Clicked</button>")
 	})
 
+	http.HandleFunc("/colors", func(w http.ResponseWriter, r *http.Request) {
+		color := r.URL.Query().Get("color")
+		if color == "" || color == "green" {
+			color = "red"
+		} else if color == "red" {
+			color = "blue"
+		} else {
+			color = "green"
+		}
+		fmt.Fprintf(w, `<div id="color-demo" class="smooth" style="color:%[1]s" hx-get="/colors?color=%[1]s" hx-swap="outerHTML" hx-trigger="every 1s"> <h1>Hello World :D</h1> </div>`, color)
+	})
+
+	http.HandleFunc("/square", func(w http.ResponseWriter, r *http.Request) {
+		x := r.URL.Query().Get("x")
+		y := r.URL.Query().Get("y")
+
+		if x == "50" && y == "50" {
+			y = "150"
+		} else if x == "50" && y == "150" {
+			x = "150"
+		} else if x == "150" && y == "150" {
+			y = "50"
+		} else { 
+			x = "50"
+		}
+			
+		fmt.Fprintf(w, `
+			<div id="dot-demo" class="dot smooth" style="top:%[1]spx;left:%[2]spx;"
+            hx-get="/square?x=%[1]s&y=%[2]s" hx-swap="outerHTML" hx-trigger="every 2s"></div>
+		`, x, y)
+	})
+
 	dir := http.Dir("./static")
 	fs := http.FileServer(dir)
 
